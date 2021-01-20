bad2b8f2c4a2decea63957ebf9397c2669155ca6
dhiegoferreira <53379935+dhiegoferreira@users.noreply.github.com>
2021-01-20 12:51:21 -0300

    Update EscalaPortugues.py

--- EscalaPortugues.py
+++ EscalaPortugues.py
@@ -1,15 +1,15 @@
-Mat = float(input("Nota de Matemática: "));  //Recebe a nota de Matemática.
-Lig = float(input("Nota de Lingugagens: ")); //Recebe a nota de Linguagens.
-Red = float(input("Nota da Redação: ")) //Recebe a nota de Redação.
+Mat = float(input("Nota de Matemática: "));  #Recebe a nota de Matemática.
+Lig = float(input("Nota de Lingugagens: ")); #Recebe a nota de Linguagens.
+Red = float(input("Nota da Redação: ")) #Recebe a nota de Redação.
 
 
-Mat = Mat*0.8 // 80% da nota de matemática 
-Lig = Lig*0.1 // 10% da nota de linguagens
-Red = Red*0.1 // 10% da nota de redação
+Mat = Mat*0.8 # 80% da nota de matemática 
+Lig = Lig*0.1 # 10% da nota de linguagens
+Red = Red*0.1 # 10% da nota de redação
 
-SomaNota = float(Mat + Lig + Red) //Soma as notas das três áreas.
+SomaNota = float(Mat + Lig + Red) #Soma as notas das três áreas.
 
-CalculaNota = (SomaNota*20)/1000 //Formula da escala portuguesa.
+CalculaNota = (SomaNota*20)/1000 #Formula da escala portuguesa.
 
 print("=========================");
-print(CalculaNota); //Saida do resultado.
+print(CalculaNota); #Saida do resultado.
